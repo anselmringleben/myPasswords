cfeabc0a52c4d54e3f64aa472dae8aa52e1d4a1a
anselmringleben <anselm.ringleben@accenture.com>
2015-03-11 09:51:21 +0100

    #3: Login page now displays error message

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -25,33 +25,8 @@
 
 						$locationProvider.hashPrefix('!');
 
-						var interceptor = function($rootScope, $q, $location) {
-							function success(response) {
-								return response;
-							}
-
-							function error(response) {
-								var status = response.status;
-								var config = response.config;
-								var method = config.method;
-								var url = config.url;
-
-								if (status == 401) {
-									$location.path("/login");
-								} else {
-									$rootScope.error = method + " on " + url
-											+ " failed with status " + status;
-								}
-
-								return $q.reject(response);
-							}
-
-							return function(promise) {
-								return promise.then(success, error);
-							};
-						};
-
-						$httpProvider.interceptors.push(interceptor);
+						$httpProvider.interceptors
+								.push('errorResponseInterceptor');
 
 					} ]).run(
 			function($rootScope, $http, $location, LoginService) {
--- src/main/resources/static/js/services.js
+++ src/main/resources/static/js/services.js
@@ -11,3 +11,31 @@ services.factory('LoginService', function($resource) {
 		}
 	});
 });
+
+services.factory('errorResponseInterceptor', [
+		'$rootScope',
+		'$q',
+		'$location',
+		function($rootScope, $q, $location) {
+			var errorResponseInterceptor = {
+				'response' : function(response) {
+					return response;
+				},
+				'responseError' : function(response) {
+					var status = response.status;
+					var config = response.config;
+					var method = config.method;
+					var url = config.url;
+
+					if (status == 401) {
+						$location.path("/login");
+					} else {
+						$rootScope.error = method + " on " + url
+								+ " failed with status " + status;
+					}
+
+					return $q.reject(response);
+				}
+			};
+			return errorResponseInterceptor;
+		} ]);
